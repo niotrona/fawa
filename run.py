1ca230e88b0e47d6c6505d2d9714b7fe746ca4cf
niotrona <135283884+niotrona@users.noreply.github.com>
2025-04-16 06:44:53 +0100

    Update run.py

--- run.py
+++ run.py
@@ -2,22 +2,17 @@ from fastapi import FastAPI, Request
 from fastapi.responses import StreamingResponse
 import httpx
 
-main_app = FastAPI()
+app = FastAPI()
 
-@main_app.get("/mediaflow-proxy")
-async def proxy(request: Request, url: str):
-    headers = {
+@app.get("/stream")
+async def proxy_stream():
+    STREAM_URL = "https://fi.okarisa.cfd/hls/FFR.m3u8"
+    HEADERS = {
         "Referer": "http://www.fawanews.com/",
-        "Origin": "http://www.fawanews.com",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+        "Origin": "http://www.fawanews.com"
     }
-
-    try:
-        async with httpx.AsyncClient() as client:
-            resp = await client.get(url, headers=headers, timeout=15.0)
-            return StreamingResponse(
-                iter([resp.content]),
-                media_type=resp.headers.get("content-type", "application/vnd.apple.mpegurl")
-            )
-    except Exception as e:
-        return {"error": str(e)}
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.get(STREAM_URL, headers=HEADERS)
+        return StreamingResponse(response.iter_bytes(), media_type=response.headers["Content-Type"])
